d9754c60d6ec8cb5625f02421ce6a6059e1e0ce8
Mike Mwongela <mikemwongela1025@gmail.com>
2026-07-26 03:59:16 +0300

    feat: add allowed dev origin

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["farm.server.trysalesense.online"],
 };
 
 export default nextConfig;
